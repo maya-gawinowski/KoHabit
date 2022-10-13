03170b4694f6e61b7aa1e43a2a73eafbe320e31f
maybee <78488271+maya-gawinowski@users.noreply.github.com>
2022-10-13 18:26:44 +0200

    petsitter search input by city

--- my-app/src/Components/Petsitter/PetsitterCard.jsx
+++ my-app/src/Components/Petsitter/PetsitterCard.jsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+import { useState } from "react";
 import Box from '@mui/material/Box';
 import defaultpic from '../../Assets/defaultpicture.png';
 
@@ -24,6 +26,7 @@ const petsitters=[
 ]
 
 function PetsitterCard(){
+    const [cityname, setCityName] = useState();
 
     function getData(){
         //here fetch the data to put into the cards
@@ -33,8 +36,22 @@ function PetsitterCard(){
         //add the demand to the user linked to this id
     }
 
+    function handleSubmitCitySearch(value){
+        //select all petsitters from this city
+        //display them
+    }
+
     return(
         <Box>
+            <div>
+                <label>
+                    City Name 
+                    <input type="text" name="nameConnection" onChange={(e) => setCityName(e.target.value)} required/>
+                </label>
+                <button onClick={handleSubmitCitySearch}>
+                    Search
+                </button>
+            </div>
             {petsitters.map((data,i) => (
                 <Box sx={{boxShadow: 3, margin: '50px'}}>
                     <Box sx={{display: 'flex', flexDirection: 'row' }}>
